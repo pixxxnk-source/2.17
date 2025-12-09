95d3f618bd56806455d650c2eecd1b3881173b15
奎京 森岡 <pi.xxx.nk@gmail.com>
2025-12-09 14:37:04 +0900

    プロジェクトファイルを追加。

---
+++ 課題2.17/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        //Hello Worldを出力する
+        Console.WriteLine("Hello, World!");
+
+        //
+        Console.Write("文字列１を入力：");
+        string str1 = Console.ReadLine();
+
+        Console.Write("文字列２を入力：");
+        string str2 = Console.ReadLine();
+
+        string result = str1 + str2;
+        Console.WriteLine("出力：" + result);
+    }
+}
